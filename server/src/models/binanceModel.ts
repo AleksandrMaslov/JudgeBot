43fab45229c30f5b2f3e238121879e2a36631a8e
AleksandrMaslov <amaslov8910@gmail.com>
2023-05-18 09:32:10 +0300

    refactor with exchangeModel extension

--- server/src/models/binanceModel.ts
+++ server/src/models/binanceModel.ts
@@ -1,89 +1,48 @@
-import axios from 'axios'
-import WebSocket from 'ws'
+import { ExchangeModel } from './base/exchangeModel.js'
+import { Symbol, BinanceTicker } from '../types'
 
-import { BinanceTicker } from '../types'
-
-export class BinanceModel {
-  public tickers: any
-  public symbols: string[]
-
-  private socket: WebSocket
-
+export class BinanceModel extends ExchangeModel {
   constructor() {
-    this.tickers = {}
-    this.symbols = []
+    super()
 
-    this.getSymbols()
+    this.symbolsUrl = 'https://api.binance.com/api/v3/exchangeInfo'
+    this.wsConnectionUrl = 'wss://stream.binance.com:9443/ws'
+    this.senderPrefix = this.constructor.name
+    this.isDebugMode = true
 
-    this.socket = new WebSocket(`wss://stream.binance.com:9443/ws`)
-    this.defineSocketListeners()
+    this.init()
   }
 
-  private getSymbols = async (): Promise<void> =>
-    await axios
-      .get('https://api.binance.com/api/v3/exchangeInfo')
-      .then((response) => {
-        const {
-          data: { symbols },
-        } = response
+  parseSymbolResponse(response: any): any[] {
+    const {
+      data: { symbols },
+    } = response
 
-        const validSymbols = symbols.filter((s: any) => s.status === 'TRADING')
+    return this.getValidSymbols(symbols)
+  }
 
-        this.symbols = validSymbols.map((s: any) => s.symbol)
+  getValidSymbols(symbolsData: any[]): any[] {
+    return symbolsData.filter((s: any) => s.status === 'TRADING')
+  }
 
-        validSymbols.map((s: any) => {
-          const symbol = s.symbol
-          this.tickers[symbol] = {
-            symbol: symbol,
-            base: s.baseAsset,
-            quote: s.quoteAsset,
-            askPrice: 0,
-            askQty: 0,
-            bidPrice: 0,
-            bidQty: 0,
-          }
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-
-  private processData = (tickers: BinanceTicker[]): void => {
-    try {
-      tickers.map((ticker: BinanceTicker) => {
-        const symbol = ticker.s
-        const askPrice = parseFloat(ticker.a)
-        const askQty = parseFloat(ticker.A)
-        const bidPrice = parseFloat(ticker.b)
-        const bidQty = parseFloat(ticker.B)
-
-        this.tickers[symbol].askPrice = askPrice
-        this.tickers[symbol].askQty = askQty
-
-        this.tickers[symbol].bidPrice = bidPrice
-        this.tickers[symbol].bidQty = bidQty
-      })
-    } catch (error) {
-      console.log(error)
+  parseTicker(symbolData: any): Symbol {
+    return {
+      symbol: symbolData.symbol,
+      base: symbolData.baseAsset,
+      quote: symbolData.quoteAsset,
+      askPrice: 0,
+      askQty: 0,
+      bidPrice: 0,
+      bidQty: 0,
     }
   }
 
-  private defineSocketListeners = (): void => {
-    this.socket.onopen = () => this.onSocketOpen()
-    this.socket.onerror = () => this.onSocketError()
-    this.socket.onclose = () => this.onSocketClose()
-    this.socket.onmessage = (event: WebSocket.MessageEvent) =>
-      this.onSocketMessage(event)
+  isDataMessageNotValid(messageData: any): boolean {
+    return messageData.result === null
   }
 
-  private onSocketMessage(event: WebSocket.MessageEvent): void {
-    const data = JSON.parse(event.data.toString())
-    if (data.result === null) return
-    this.processData(data)
-  }
-
-  private onSocketOpen(): void {
-    this.socket.send(
+  subscribeAllTickers(): void {
+    this.socket!.send(
       JSON.stringify({
         method: 'SUBSCRIBE',
         params: ['!ticker@arr'],
@@ -92,11 +51,18 @@ export class BinanceModel {
     )
   }
 
-  private onSocketClose(): void {
-    console.log('Closed')
-  }
+  updateTicker(tickerData: BinanceTicker): void {
+    const symbol = tickerData.s
 
-  private onSocketError(): void {
-    console.log('Error')
+    const askPrice = parseFloat(tickerData.a)
+    const askQty = parseFloat(tickerData.A)
+    const bidPrice = parseFloat(tickerData.b)
+    const bidQty = parseFloat(tickerData.B)
+
+    this.tickers[symbol].askPrice = askPrice
+    this.tickers[symbol].askQty = askQty
+
+    this.tickers[symbol].bidPrice = bidPrice
+    this.tickers[symbol].bidQty = bidQty
   }
 }
