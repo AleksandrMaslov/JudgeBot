fed66fc2f10e0a479ff661a7bba7b49cde07d62e
AleksandrMaslov <amaslov8910@gmail.com>
2023-05-20 13:02:07 +0300

    add undefined

--- server/src/models/ticker.ts
+++ server/src/models/ticker.ts
@@ -2,23 +2,24 @@ import { SymbolData } from '../types'
 
 export class Ticker {
   public symbol: string
-  public base: string
-  public quote: string
-  public askPrice: number
-  public askQty: number
-  public bidPrice: number
-  public bidQty: number
+  public base: string | undefined
+  public quote: string | undefined
+  public askPrice: number | undefined
+  public askQty: number | undefined
+  public bidPrice: number | undefined
+  public bidQty: number | undefined
 
   constructor(symbolData: SymbolData) {
     const { symbol, base, quote } = symbolData
 
     this.symbol = symbol
+
     this.base = base
     this.quote = quote
 
-    this.askPrice = 0
-    this.askQty = 0
-    this.bidPrice = 0
-    this.bidQty = 0
+    this.askPrice = undefined
+    this.askQty = undefined
+    this.bidPrice = undefined
+    this.bidQty = undefined
   }
 }
