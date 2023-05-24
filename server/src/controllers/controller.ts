7e9620f03a42bfe372b281bee900a94187aac935
AleksandrMaslov <amaslov8910@gmail.com>
2023-05-24 20:04:43 +0300

    add test proffit calculator logging

--- server/src/controllers/controller.ts
+++ server/src/controllers/controller.ts
@@ -1,6 +1,7 @@
 import { ExchangeModel } from '../models/exchanges/base/exchangeModel.js'
 import { BinanceModel } from '../models/exchanges/binanceModel.js'
 import { KucoinModel } from '../models/exchanges/kucoinModel.js'
+import { Ticker } from '../models/ticker.js'
 
 export class Controller {
   private binance: BinanceModel
@@ -19,6 +20,37 @@ export class Controller {
 
   private process() {
     const asset = 'USDT'
-    this.binance.getTradeablePairsWith(this.kucoin, asset)
+    const cases = this.binance.getCasesWith(this.kucoin, asset)
+    this.calculate(cases)
+  }
+
+  private calculate(tradeablePairs: any[][]): void {
+    let spreads = []
+    for (const pair of tradeablePairs) {
+      const [pairAsset, ticker, pairTicker] = pair
+
+      let proffitA: number
+      let proffitB: number
+      if (ticker.base === pairTicker.base) {
+        const spreadA = ticker.askPrice! - pairTicker.bidPrice!
+        proffitA = parseFloat(((spreadA * 100) / ticker.askPrice!).toFixed(2))
+        const spreadB = pairTicker.askPrice! - ticker.bidPrice!
+        proffitB = parseFloat(
+          ((spreadB * 100) / pairTicker.askPrice!).toFixed(2)
+        )
+      } else {
+        console.log('different!', ticker.base, pairTicker.base)
+        // FIX
+        const spreadA = ticker.askPrice! - pairTicker.bidPrice!
+        proffitA = 0 //parseFloat(((spreadA * 100) / ticker.askPrice!).toFixed(2))
+        const spreadB = pairTicker.askPrice! - ticker.bidPrice!
+        proffitB = 0 //parseFloat( ((spreadB * 100) / pairTicker.askPrice!).toFixed(2) )
+      }
+
+      if (proffitA > 2 || proffitB > 2)
+        spreads.push([pairAsset, proffitA, proffitB])
+    }
+    console.log(spreads.length)
+    console.log(spreads)
   }
 }
