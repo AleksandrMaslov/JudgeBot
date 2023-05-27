08d34a38ac96841a0a7d4d635b115f4d3d5e1c01
AleksandrMaslov <amaslov8910@gmail.com>
2023-05-27 21:50:27 +0300

    remove unsused url

--- server/src/models/exchanges/bybitModel.ts
+++ server/src/models/exchanges/bybitModel.ts
@@ -15,7 +15,6 @@ export class BybitModel extends ExchangeModel {
   constructor() {
     super()
 
-    this.symbolsUrl = ''
     this.tickersUrl = 'https://api.bybit.com/v5/market/tickers?category=spot'
     this.wsConnectionUrl = 'wss://stream.bybit.com/v5/public/spot'
     this.tickersTopic = 'orderbook.1.'
